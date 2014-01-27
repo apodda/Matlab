9d72121210f212d614fb2642056c11e53c4f6434
Alessio Podda <alessio@localhost.localdomain>
2014-01-27 12:27:40 +0100

    Script for the heat equation Added a script that solves and graphs the heat equation in 2D, using Crank-Nicolson discretization, and BICGSTAB to solve the linear system.
    The setup for the Laplace equation has been reused.

---
+++ Parabolic_2D.m
@@ -0,0 +1,39 @@
+% 2D Parabolic Equation
+% (- d_t + b * d_xx + a * d_x + c) u = 0
+
+source setup.m
+
+%Crank-Nicolson [hint: this will take forever...]
+U_pruned = zeros(nnz(not(pattern)), length(time_vector));
+
+lhs = (id_mat - 0.5 * k / (h^2) * laplacian);
+rhs = (id_mat + 0.5 * k / (h^2) * laplacian);
+
+lhs_pruned = lhs(not(pattern), not(pattern));
+rhs_pruned = rhs(not(pattern), not(pattern));
+
+constant_term = k / (h^2) * laplacian(not(pattern), pattern) * initial_conditions_vector(pattern);
+
+U_pruned(:,1) = initial_conditions_vector(not(pattern));
+
+for i=2:length(time_vector)
+  U_pruned(:, i) = bicgstab(lhs_pruned, (rhs_pruned * U_pruned(:, i-1)) + constant_term);
+  %U_pruned(:, i) = k * b * laplacian(not(pattern), not(pattern))* U_pruned(:, i-1) + U_pruned(:, i-1) + k * constant_term;
+end
+
+U = zeros(length(pattern), length(time_vector));
+
+for i=1:length(time_vector)
+  U(:, i) = initial_conditions_vector;
+end
+
+U(not(pattern), :) = U_pruned;
+
+for i=1:length(time_vector)
+  mesh(grid_x, grid_y, reshape(U(:, i), m, m));
+  axis([-1 barrier -1 barrier 0 250])
+  %print(sprintf("frame%i.png", i), "-dpng")
+  pause(time_vector(i))
+end
+
+input("Press any key to continue ..."); 
